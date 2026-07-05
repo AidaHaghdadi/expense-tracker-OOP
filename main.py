1292dc71f5fd1c8714c3eb448a6bbacbbd5c997f
Aida <aida.hg0483@gmail.com>
2026-07-05 18:10:09 +0330

    Implement show expenses method

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 
-class Expense : # Responsible for maintaining information about an expense.
-    def __init__ (self, expense_title, expense_amount, expense_date) :
-        self.title = expense_title
-        self.amount = expense_amount
-        self.date = expense_date
+class Expense: # Responsible for maintaining information about an expense.
+    def __init__ (self, title, amount, date):
+        self.title = title
+        self.amount = amount
+        self.date = date
 
-class ExpenseTracker : # Responsible for managing collections of expenses.
-    def __init__ (self) :
+class ExpenseTracker: # Responsible for managing collections of expenses.
+    def __init__ (self):
         self.expenses = []
 
     # Add expense function
     def add_expense(self, expense):
         self.expenses.append(expense)
 
+    # Show expenses function
+    def show_expenses(self):
+        if not self.expenses:
+            print("No expense found!")
+        else :
+            print("\nExpenses list:")
+            for index, expense in enumerate(self.expenses): # `expense` is an instance of `Expense`.
+                print(f"{index + 1}- Title: {expense.title} | Amount: {expense.amount} | Date: {expense.date}")
