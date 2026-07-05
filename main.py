02b6d810f6a6169508898ad10e790f4e3eb37b23
Aida <aida.hg0483@gmail.com>
2026-07-05 17:53:02 +0330

    Implement add expense method

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
-class Expense :
+class Expense : # Responsible for maintaining information about an expense.
     def __init__ (self, expense_title, expense_amount, expense_date) :
         self.title = expense_title
         self.amount = expense_amount
         self.date = expense_date
 
-class ExpenseTracker :
+class ExpenseTracker : # Responsible for managing collections of expenses.
     def __init__ (self) :
         self.expenses = []
+
+    # Add expense function
+    def add_expense(self, expense):
+        self.expenses.append(expense)
+
