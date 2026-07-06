b515fa597287287ed3ec62d0f1cf0e13701a87fd
Aida <aida.hg0483@gmail.com>
2026-07-06 19:03:38 +0330

    Implement total and average calculation

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ class ExpenseTracker: # Responsible for managing collections of expenses.
     # Show expenses function
     def show_expenses(self):
         if not self.expenses:
-            print("No expense found!")
-        else :
-            print("\nExpenses list:")
-            for index, expense in enumerate(self.expenses): # `expense` is an instance of `Expense`.
-                print(f"{index + 1}- Title: {expense.title} | Amount: {expense.amount} | Date: {expense.date}")
+            return None
+        
+        print("\nExpenses list:")
+        for index, expense in enumerate(self.expenses): # `expense` is an instance of `Expense`.
+            print(f"{index + 1}- Title: {expense.title} | Amount: {expense.amount} | Date: {expense.date}")
 
     # Search expense function
     def search_expense(self, search_title):
@@ -29,3 +29,19 @@ class ExpenseTracker: # Responsible for managing collections of expenses.
             if search_title == expense.title:
                 results.append(expense)
         return results
+
+    # Calculate total function
+    def calculate_total(self):
+        total_amount = 0
+        for expense in self.expenses :
+            total_amount += expense.amount
+        return total_amount
+
+    # Calculate average function
+    def calculate_average(self):
+        if not self.expenses :
+            return None
+
+        total_amount = self.calculate_total()
+        average_amount = total_amount / len(self.expenses)
+        return average_amount
